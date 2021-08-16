123f0967672ce52773a978a4dadcb47ed99d421a
Ilya Vishinsky <ivyishinskiy@cloudlinux.com>
2021-08-16 18:01:33 +0300

    Web application

--- site/src/handlers.js
+++ site/src/handlers.js
@@ -2,9 +2,67 @@ import 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js'
 import { Editor } from 'https://cdn.skypack.dev/@tiptap/core?min'
 import StarterKit from 'https://cdn.skypack.dev/@tiptap/starter-kit?min'
 import '../node_modules/js-sha256/build/sha256.min.js'
+import './jquery.cookie.js'
+import { adress_api } from './config.js'
 
 
 var hash = sha256.create();
+var access_token = "";
+var token_type = "bearer";
+var item_id = null;
+var item_created = ""
+var item_name = ""
+
+
+$.ajaxSetup({
+	headers: { 
+		"Accept": "application/json",
+	}
+});
+
+
+function html_to_text(data, last_n=true) {
+	data = data.replace(/<style([\s\S]*?)<\/style>/gi, '');
+	data = data.replace(/<script([\s\S]*?)<\/script>/gi, '');
+	data = data.replace(/<\/div>/ig, '\n');
+	data = data.replace(/<\/li>/ig, '\n');
+	data = data.replace(/<li>/ig, '  * ');
+	data = data.replace(/<\/ul>/ig, '\n');
+	data = data.replace(/<p>/ig, '');
+	data = data.replace(/<\/p>/ig, '\n');
+	data = data.replace(/<br\s*[\/]?>/gi, '\n');
+	data = data.replace(/<[^>]+>/ig, '');
+	if (last_n == true) {
+		data = data.replace(/\n+$/m, '');
+	}
+	else if (data.slice(-1) == '\n') {
+		data = data.slice(0, -1);
+	}
+	data = data.replace(/\\n/ig, '\n');
+	return data;
+}
+
+function text_to_html(data) {
+	data = data.replace(/\n/gi, '</p><p>');
+	data = data.replace(/\t/gi, '    ');
+	data = '<p>' + data + '</p>';
+	return data
+}
+
+function get_info(res) {
+	var info = "";
+	if (res != null) {info = info + "<p>success = " + String(res.success) + "</p>";}
+	info = info + "<p>item_id = " + item_id + "</p>";
+	info = info + "<p>created = " + item_created + "</p>";
+	if (res != null) {info = info + "<p>time = " + res.time_query + "</p>";}
+	return info;
+}
+
+function get_info_success(res) {
+	var info = "";
+	if (res != null) {info = info + "<p>success = " + String(res.success) + "</p>";}
+	return info;
+}
 
 
 const editor_thema = new Editor({
@@ -43,21 +101,28 @@ const editor_liner = new Editor({
 })
 
 
-const adress_api = "http://127.0.0.1:8000";
-var access_token = "";
-var token_type = "bearer";
-var item_id = null;
-
-
-$.ajaxSetup({
-	headers: { 
-		"Accept": "application/json",
-		"Authorization": token_type + " " + access_token,
-	}
-});
-
-
 $(document).ready(function() {
+	access_token = $.cookie('access_token');
+	token_type = $.cookie('token_type'); console.log("Authorization: " + token_type + " " + access_token);
+	item_id = $.cookie('item_id'); console.log('item_id=' + item_id);
+	item_created = $.cookie('item_created'); console.log('item_created=' + item_created);
+	item_name = $.cookie('item_name'); console.log('item_name=' + item_name);
+	
+	if (access_token != null && token_type != null) {
+		document.getElementById("generator").style.display = 'block';
+		document.getElementById("err_auth").style.display = 'none';
+		$.ajaxSetup({
+			headers: { 
+			"Accept": "application/json",
+			"Authorization": token_type + " " + access_token,
+			}
+		});
+	}
+	if (item_id != null && item_name != null && item_created != null) {
+		document.getElementById("result").innerHTML = get_info(null);
+		editor_thema.commands.setContent(text_to_html(item_name));
+	}
+	
 	$('button.text_create').click(function(e) {
 		e.preventDefault();  // Stop form from sending request to server
 		var btn = $(this);
@@ -68,13 +133,14 @@ $(document).ready(function() {
 			dataType: "json",
 			crossDomain: true,
 			data: {
-				"name": editor_thema.getHTML(),
+				"name": html_to_text(editor_thema.getHTML()),
 			},
 			success: function(res) {
 				console.log(res);
-				item_id = res.id;
-				var info = "<p>item_id = " + item_id + "</p><p>created = " + res.created + "</p>";
-				document.getElementById("result").innerHTML = info;
+				item_id = res.id; $.cookie('item_id', item_id);
+				item_created = res.created; $.cookie('item_created', item_created);
+				item_name = html_to_text(editor_thema.getHTML()); $.cookie('item_name', item_name);
+				document.getElementById("result").innerHTML = get_info(null);
 			},
 			error: function(er) {console.log(er);},
 		});
@@ -90,17 +156,21 @@ $(document).ready(function() {
 			dataType: "json",
 			crossDomain: true,
 			data: {
-				"text": editor_text.getHTML(),
-				"text_next": editor_next.getHTML(),
-				"liner": editor_liner.getHTML(),
+				"text": html_to_text(editor_text.getHTML(), false),
+				"text_next": html_to_text(editor_next.getHTML(), false),
+				"liner": html_to_text(editor_liner.getHTML()),
 				"text_type": "No style",
 			},
 			success: function(res) {
 				console.log(res);
 				if (res.success == true) {
-					editor_text.commands.setContent(res.text);
-					editor_next.commands.setContent(res.next);
+					editor_text.commands.setContent(text_to_html(res.text));
+					editor_next.commands.setContent(text_to_html(res.next));
 					editor_liner.commands.setContent("");
+					document.getElementById("result").innerHTML = get_info(res);
+				}
+				else {
+					document.getElementById("result").innerHTML = get_info_success(res);
 				}
 			},
 			error: function(er) {console.log(er);},
@@ -117,15 +187,19 @@ $(document).ready(function() {
 			dataType: "json",
 			crossDomain: true,
 			data: {
-				"text": editor_text.getHTML(),
-				"text_next": editor_next.getHTML(),
-				"liner": editor_liner.getHTML(),
+				"text": html_to_text(editor_text.getHTML(), false),
+				"text_next": html_to_text(editor_next.getHTML(), false),
+				"liner": html_to_text(editor_liner.getHTML()),
 				"text_type": "No style",
 			},
 			success: function(res) {
 				console.log(res);
 				if (res.success == true) {
-					editor_next.commands.setContent(res.next);
+					editor_next.commands.setContent(text_to_html(res.next));
+					document.getElementById("result").innerHTML = get_info(res);
+				}
+				else {
+					document.getElementById("result").innerHTML = get_info_success(res);
 				}
 			},
 			error: function(er) {console.log(er);},
@@ -142,18 +216,22 @@ $(document).ready(function() {
 			dataType: "json",
 			crossDomain: true,
 			data: {
-				"text": editor_text.getHTML(),
-				"text_next": editor_next.getHTML(),
-				"liner": editor_liner.getHTML(),
+				"text": html_to_text(editor_text.getHTML(), false),
+				"text_next": html_to_text(editor_next.getHTML(), false),
+				"liner": html_to_text(editor_liner.getHTML()),
 				"text_type": "No style",
 			},
 			success: function(res) {
 				console.log(res);
 				if (res.success == true) {
-					editor_text.commands.setContent(res.text);
+					editor_text.commands.setContent(text_to_html(res.text));
 					editor_next.commands.setContent("");
 					editor_liner.commands.setContent("");
-					item_id = null;
+					item_id = null; $.removeCookie(item_id);
+					item_created = ""; $.removeCookie(item_created);
+					item_name = null; $.removeCookie(item_name);
+					editor_thema.commands.setContent("");
+					document.getElementById("result").innerHTML = "";
 				}
 			},
 			error: function(er) {console.log(er);},
@@ -182,18 +260,30 @@ $(document).ready(function() {
 			success: function(res) {
 				console.log(res);
 				document.getElementById("generator").style.display = 'block';
-				access_token = res.access_token;
-				token_type = res.token_type;
+				document.getElementById("err_auth").style.display = 'none';
+				access_token = res.access_token; $.cookie('access_token', access_token);
+				token_type = res.token_type; $.cookie('token_type', token_type);
+				$.ajaxSetup({
+					headers: { 
+					"Accept": "application/json",
+					"Authorization": token_type + " " + access_token,
+					}
+				});
 			},
-			error: function(er) {console.log(er);},
+			error: function(er) {
+				console.log(er);
+				document.getElementById("err_auth").style.display = 'block';
+			},
 		});
 	});
 	
 	$('button.logout').click(function(e) {
 		e.preventDefault();  // Stop form from sending request to server
-		access_token = "";
-		token_type = "bearer";
-		item_id = null;
+		access_token = ""; $.removeCookie(access_token);
+		token_type = "bearer"; $.removeCookie(token_type);
+		item_id = null; $.removeCookie(item_id);
+		item_created = ""; $.removeCookie(item_created);
 		document.getElementById("generator").style.display = 'none';
+		document.getElementById("err_auth").style.display = 'none';
 	});
 });
